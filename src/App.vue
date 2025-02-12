27c25b254f86cc68e68da64cddbb0a7915622e7d
Gabriel Gonzalez <ggonz296@stu.southtexascollege.edu>
2025-02-11 22:24:23 -0600

    Added Vue Files and Created Arrays for games and cart. Also Completed TotalGames.vue file.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from './components/Header.vue';
+  import TotalGames from './components/TotalGames.vue';
+  import AddGames from './components/AddGames.vue';
+  import {ref, computed} from 'vue'
+
+  //My array of games - These are pre-initalized
+  const myGames = ref([
+    {id: 1, title: "Older Rings", price: 60},
+    {id: 2, title: "Monsters", price: 40},
+    {id: 3, title: "DC Rivals", price: 50},
+  ])
+
+  //My array of items in a cart
+  const cart = ref([])
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Header></Header>
+  <AddGames></AddGames>
+  <TotalGames :total="totalPrice"></TotalGames>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
