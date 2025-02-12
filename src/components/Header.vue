084e22e1efc9a8936be6c0eb377926493abe3e29
Gabriel Gonzalez <ggonz296@stu.southtexascollege.edu>
2025-02-11 22:25:49 -0600

    Initalized files, created arrays, and completed TotalGames.vue files

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Gabriel's Game Store</h1>
+    <h2>For CITP-4316-V01</h2>
+    <h2>Professor Perez</h2>
+</template>
---
+++ src/components/TotalGames.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import{defineProps} from 'vue'
+    const props = defineProps({
+        total:{
+            type: Number,
+            required: true,
+        }
+    })
+</script>
+
+<template>
+    <h2>Total Price: ${{ total }}</h2>
+</template>
